5f4052ae3d827139bfc81abd677fe6c42abb3fc6
Tiago Neves <tiagonevesx9@gmail.com>
2025-07-28 14:04:10 +0100

    Update ExpenseModel.cs

--- Models/ExpenseModel.cs
+++ Models/ExpenseModel.cs
@@ -4,23 +4,29 @@
  */
 public class ExpenseModel
 {
+
     public int Id { get; set; }
     public string Description { get; set; }
     public decimal Amount { get; set; }
+    public TransactionType Type => Amount >= 0 ? TransactionType.Income : TransactionType.Expense;
     public DateTime Date { get; set; }
     public Category Category { get; set; }
     public string? Notes { get; set; }
-
+    private static int lastId = 0; // Static variable to keep track of the last used ID
     private static int getNextId()
     {
         // This method return the next ID for the expense
         // In a real application, this would likely query a database or use a static counter
         // For simplicity, we will just return a random number here
-        return new Random().Next(1, 10000);
+
+        return lastId++;
     }
 
     public ExpenseModel(string description, decimal amount, Category category, string? notes)
     {
+        if (amount == 0)
+            throw new ArgumentException("Amount cannot be zero");
+
         Id = getNextId();
         Description = description;
         Amount = amount;
@@ -28,4 +34,10 @@ public class ExpenseModel
         Category = category;
         Notes = notes;
     }
+    
+    public enum TransactionType
+{
+    Income,
+    Expense
+}
 }
